fe39b08cb95abf1be1ecb8aa36755426a20fd0ed
Чечеуров Игорь Дмитриевич <ichecheurov@stan-group.com>
2023-10-24 14:27:04 +0300

    feat: add two more array-tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -194,8 +194,8 @@ function getTail(arr, n) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  return arr.map((index) => `${index.join(',')}`).join('\n');
 }
 
 /**
@@ -209,8 +209,8 @@ function toCsvText(/* arr */) {
  *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
-function toArrayOfSquares(/* arr */) {
-  throw new Error('Not implemented');
+function toArrayOfSquares(arr) {
+  return arr.map((item) => item * item);
 }
 
 /**
