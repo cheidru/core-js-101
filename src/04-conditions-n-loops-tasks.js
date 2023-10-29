85975f1faf1589fcb6aefe8f44719e50ecf23d4d
Igor Checheurov <chei@yandex.ru>
2023-10-29 22:11:29 +0300

    fix: fix system crash

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -294,13 +294,14 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(num) {
-  let numstr = num.toString();
-  while (numstr.length > 1) {
-    const arr = numstr.split('');
-    numstr = arr.reduce((sum, item) => sum + item).toString();
-  }
-  return Number(numstr);
+function getDigitalRoot(/* num */) {
+  throw new Error('Not implemented');
+  // let numstr = num.toString();
+  // while (numstr.length > 1) {
+  //   const arr = numstr.split('');
+  //   numstr = arr.reduce((sum, item) => sum + item).toString();
+  // }
+  // return Number(numstr);
 }
 
 /**
